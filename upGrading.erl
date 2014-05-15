55e4c43a25d67bd7da618c0681abd767637e4dab
haetze <richy.sting@gmail.com>
2014-05-15 20:18:29 +0200

    doc

--- upGrading.erl
+++ upGrading.erl
@@ -5,21 +5,27 @@
 -export([start/1, rec/0, sen/1, sen/2]).
 -vsn(1.1).
 
+-spec(start( integer() ) -> integer()).
 start(N)->
 	N+1.
+-spec( rec() -> ok).
 rec()-> 
 	receive
 		_ -> io:format("qqqqqq")
 	end,
-	rec().
+	rec(),
+	ok.
 
+-spec(sen(pid()) -> ok).
 sen(Pid)->
 	sen(0, Pid).
+-spec(sen(integer(), pid()) -> ok).
 sen(10000000, Pid)->
 	Pid ! 10,
 	sen(0, Pid);
 sen(N, Pid)->
-	sen(N+1, Pid).
+	sen(N+1, Pid),
+	ok.
 
 
 
