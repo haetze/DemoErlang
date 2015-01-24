31c88af0675ff6674de0c3ac3b1237c94432a51c
Richard Stewing <richy.sting@gmail.com>
2015-01-24 06:50:21 -0600

    demo

--- demo.erl
+++ demo.erl
@@ -1,8 +1,12 @@
 -module(demo).
--export([double/1]).
+-export([double/1, add/2, double/2]).
 
-
+double(X, Y)->
+	{times(X,2),times(Y,2)}.
 double(Value)->
 	times(Value, 2).
 times(X, Y)->
 	X*Y.
+
+add(X, Y) ->
+	X+Y.
