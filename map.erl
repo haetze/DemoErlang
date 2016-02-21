a4cac4bd3428f33b0a9bd709ad56f3c23fe278e0
Richard Stewing <richy.sting@gmail.com>
2016-02-20 20:35:39 -0800

    some test

--- map.erl
+++ map.erl
@@ -1,11 +1,31 @@
--module (test).
--export ([fuc/1]).
+%%@author Richard Stewing <richy.sting@gmail.com>
+%%@date 20, 02, 2015
+%%@copyright Richard Stewing
+%%@version 0.1
+%%@doc aklsxmakmlxmalsxmalsx
 
+
+-module (map).
+
+-export ([fuc/1, mapper/2]).
+
+%example program 
+-spec(fuc(integer()) -> integer()).
 fuc(Ob)->
-if 
+    if 
 	is_integer(12/Ob)->
-		Ob;
-	is_float(12/Ob)->
-		no
+	    Ob;
+       	%is_float(12/Ob)->
+	   % no;
+	true  ->
+	    Ob*2
 
-end.
+    end.
+
+%self implemented map function
+-spec(mapper([term()], fun((term()) -> term())) -> [term()]).
+mapper([], _) -> 
+    [];
+mapper([H|T], F) -> 
+    [F(H)| mapper(T, F)].
+    
