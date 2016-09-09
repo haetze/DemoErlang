4fcbfe63bd8e03affe53e60f289efabe477812fc
Richard Stewing <richy.sting@gmail.com>
2016-09-09 11:56:35 -0700

    range function test

--- bump.erl
+++ bump.erl
@@ -1,8 +1,17 @@
 -module (bump).
--export ([bump/1, eq/2]).
+-export ([bump/1, eq/2, range/2, range/1]).
 
 bump([])-> [];
 bump([Head| Tail])-> [Head +1 | bump(Tail) ].
+
+range(N) when N < 0 ->
+    range(N, 0);
+range(N) -> range(0, N).
+
+range(Low, Low) ->
+    [Low];
+range(Low, High) when Low > High  -> [];
+range(Low, High) -> [Low | range(Low+1, High)]. 
 
 
 eq(_N, _N) -> true;
